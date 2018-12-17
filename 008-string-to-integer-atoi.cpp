feaf04485b74ba9f798ecdb16967a011ebb97129
lamborghini-ubuntu <1323242382@qq.com>
2018-12-17 19:26:42 +0800

    008

--- 008-string-to-integer-atoi.cpp
+++ 008-string-to-integer-atoi.cpp
@@ -17,28 +17,26 @@ class Solution
     {
         int r = 0;
         int stautu = 0;
+        bool space = true;
         for (int i = 0; i < str.length(); i++)
         {
-            if (r == 0 && str[i] == ' ' && stautu == 0)
+            if (space && str[i] == ' ' && stautu == 0)
                 continue;
-            else if (r == 0 && stautu == 0 && str[i] == '-')
+            else if (space && stautu == 0 && str[i] == '-')
                 stautu = -1;
-            else if (r == 0 && stautu == 0 && str[i] == '+')
+            else if (space && stautu == 0 && str[i] == '+')
                 stautu = 1;
             else if (str[i] >= '0' && str[i] <= '9')
             {
+                space = false;
                 int tmp = str[i] - '0';
-                // printf("%d", tmp);
+                if (stautu == -1)
+                    tmp = -tmp;
                 if (r > max || (r == max && tmp > 7))
                     return INT_MAX;
                 if (r < min || (r == min && tmp < -8))
                     return INT_MIN;
                 r = r * 10 + tmp;
-                if (r != 0 && stautu == -1)
-                {
-                    r = -r;
-                    stautu = 3;
-                }
             }
             else
                 break;
@@ -47,14 +45,16 @@ class Solution
     }
 };
 
-int main(int argc, const char *argv[])
-{
-    Solution obj = Solution();
-    printf("%d\n", obj.myAtoi("    -42"));
-    printf("%d\n", obj.myAtoi("4193 with words"));
-    printf("%d\n", obj.myAtoi("words and 987"));
-    printf("%d\n", obj.myAtoi("-91283472332"));
-    printf("%d\n", obj.myAtoi("- 12 3"));
-    printf("%d\n", obj.myAtoi("  -123 sdf34"));
-    return 0;
-}
+// int main(int argc, const char *argv[])
+// {
+//     Solution obj = Solution();
+//     printf("%d\n", obj.myAtoi("    -42"));
+//     printf("%d\n", obj.myAtoi("4193 with words"));
+//     printf("%d\n", obj.myAtoi("words and 987"));
+//     printf("%d\n", obj.myAtoi("-91283472332"));
+//     printf("%d\n", obj.myAtoi("- 12 3"));
+//     printf("%d\n", obj.myAtoi("  -123 sdf34"));
+//     printf("%d\n", obj.myAtoi("-2147483649"));
+//     printf("%d\n", obj.myAtoi("0-1"));
+//     return 0;
+// }

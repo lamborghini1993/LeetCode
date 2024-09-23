3b3e5a59ae2408d66a785d7d2f6a19bc3f59ac0e
xiaohao <hao.xiao@narwal.com>
2024-09-23 14:39:29 +0800

    字典序的第K小数字

---
+++ 400-499/440. 字典序的第K小数字.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getSteps(self, cur: int, n: int) -> int:
+        steps, first, last = 0, cur, cur
+        while first <= n:
+            steps += min(last, n) - first + 1
+            first *= 10
+            last = last * 10 + 9
+        return steps
+
+    def findKthNumber(self, n: int, k: int) -> int:
+        cur = 1
+        k -= 1
+        while k:
+            steps = self.getSteps(cur, n)
+            if steps <= k:
+                k -= steps
+                cur += 1
+            else:
+                cur *= 10
+                k -= 1
+        return cur
+
+
+func = Solution().findKthNumber
+print(func(13, 2))
+print(func(1393, 21))
